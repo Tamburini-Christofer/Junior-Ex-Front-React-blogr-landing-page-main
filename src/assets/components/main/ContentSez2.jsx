e32bbe84707cc5a7fe599c4c9705bce796ddc7a1
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-18 13:16:00 +0200

    Creazione contenutore sezione 2

--- src/assets/components/main/ContentSez2.jsx
+++ src/assets/components/main/ContentSez2.jsx
@@ -1,6 +1,14 @@
 const ContentSez2 = () => {
     return (
-        <></>
+        <>
+        <div className="content2Img">
+            <img src="/public/img/illustration-phones.svg" alt="icona cellulare" className="smartphone"/>
+            <div className="contentSez2">
+                <h2>State of the Art Infrastructure</h2>
+                <span>With reliability and speed in mind, worldwide data centers provide the backdone for ultra-fast connectivity. This ensures your site will load instantly, no matter where your readers are, kepping your site competitive</span>
+            </div>
+        </div>
+        </>
     )
 }
 
