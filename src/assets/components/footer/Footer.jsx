1ee3726bfbfec2641835c7c5b1afde1a045637d1
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-18 13:57:20 +0200

    Creazione Footer

--- src/assets/components/footer/Footer.jsx
+++ src/assets/components/footer/Footer.jsx
@@ -1,8 +1,69 @@
 const Footer = () => {
-    return (
-        <>
-        </>
-    )
-}
+  const logo = "/public/img/logo.svg";
 
-export default Footer; 
+  const product = [
+    "Overview",
+    "Pricing",
+    "Marketplace",
+    "Features",
+    "Integrations",
+  ];
+  const company = ["About", "Team", "Blog", "Careers"];
+  const connect = ["Contact", "Newsletter", "Linkedln"];
+
+  return (
+    <>
+      <div className="footer">
+        <div className="footerGrid">
+          <div>
+            <img src={logo} alt="Logo aziendale" />
+          </div>
+          <div>
+            <nav>
+              <ul className="ulFlex">
+                <li>
+                  <h4>Product</h4>
+                </li>
+                {product.map((lista, index) => (
+                  <li keys={index}>
+                    <a href="">{lista}</a>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+          <div>
+            <nav>
+              <ul className="ulFlex">
+                <li>
+                  <h4>Company</h4>
+                </li>
+                {company.map((lista, index) => (
+                  <li keys={index}>
+                    <a href="">{lista}</a>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+          <div>
+            <nav>
+              <ul className="ulFlex">
+                <li>
+                  <h4>Connect</h4>
+                </li>
+                {connect.map((lista, index) => (
+                  <li keys={index}>
+                    <a href="">{lista}</a>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
--- src/assets/components/main/ContentSez3.jsx
+++ src/assets/components/main/ContentSez3.jsx
@@ -1,6 +1,24 @@
 const ContentSez3 = () => {
     return (
-        <></>
+        <>
+            <div className="containerImg3">
+                <div className="contentSez3">
+                    <div>
+                        <h3>Free, open, simple </h3>
+                        <span>Blogr is free and open source application backend by a large community of helpful developers. It support features such as code syntax highlighting, RSS feeds, social medias integration, thind-party commenting tools, and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn
+
+                        </span>
+                    </div>
+                    <div>
+                        <h3>Powerful tooling</h3>
+                        <span>
+                           Batteries include. We built a simple and straightfoward CLI tool that makes customization and deployment a breeze, but capable of producing even the most complicated sites.
+                        </span>
+                  </div>
+                </div>
+            </div>
+        
+        </>
     )
 }
 
