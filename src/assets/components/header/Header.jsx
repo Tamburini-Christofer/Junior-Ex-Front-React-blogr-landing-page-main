3e19303adee3c5ac64417cfcfa2ef8d3c4e0b6c7
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-18 12:20:26 +0200

    Creazione Header

--- src/assets/components/header/Header.jsx
+++ src/assets/components/header/Header.jsx
@@ -1,8 +1,16 @@
 const Header = () => {
-    return (
-        <>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="containerHeader">
+        <h1>A modern publishing platform</h1>
+        <h3>Grow your audience and build your online brand</h3>
+        <div>
+            <button className="btnStart">Start for Free</button>
+            <button className="btnLearn">Learn More</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Header;
