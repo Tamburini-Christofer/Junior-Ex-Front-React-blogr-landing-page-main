740cbf61d186acb675715e3748b8ea2c91ddd20e
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-18 12:42:13 +0200

    Creazione contenuto sezione 1

--- src/assets/components/main/ContentSez1.jsx
+++ src/assets/components/main/ContentSez1.jsx
@@ -1,6 +1,26 @@
 const ContentSez1 = () => {
     return (
-        <></>
+        <>
+        <div className="backgroundImg">
+            <div className="containerCont1">
+                <h2>Designed fot the future</h2>
+                <div>
+                    <div>
+                        <h3>Introducing an extensible editor </h3>
+                        <span>Blog features an exceedingly intuitive interface wich lets you focus on one thing: creating content. The editor supports management of multiple blogs and allows easy manipoulation of embeds such as images, videos, and Markdowqn. Extensibility with plugins and themes provide easy ways to add functionality or change the looks of a blog</span>
+                    </div>
+                    <div>
+                        <h3>Robust content management</h3>
+                        <span>
+                            Flexible content management enables users to easily move throught posts. Increase the usability of your blog by adding customized categories, sections, format, or flow. With this functionalitu, you're in full control.
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        
+        </>
     )
 }
 
